90f508c9d5645c263a5f3d570ae2fac1787fe80d
djqdos <richard.bartlett@gmail.com>
2025-02-05 20:25:01 +0000

    added terraform init code

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,18 @@
+
+module "resource_group" {
+  source = "./modules/resource_group"
+
+  location            = "uksouth"
+  resource_group_name = "${var.resource_group_name_prefix}-${var.resource_group_name}"
+}
+
+
+module "kubernetes_cluster" {
+  source = "./modules/kubernetes_cluster"
+
+  cluster_location     = module.resource_group.location
+  cluster_name         = "test-aks"
+  resource_group_name  = module.resource_group.name
+  service_principal_id = var.client_id
+  password             = var.client_secret
+}
---
+++ infrastructure/terraform/providers.tf
@@ -0,0 +1,26 @@
+# Define the providers that are needed
+# These will get installed when "terraform init" is ran
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+
+    #azuread = {
+    #  source = "hashicorp/azuread"
+    #  version = "~2.36.0"
+    #}
+  }
+}
+
+
+# Defines any properties or configurations for this provider
+provider "azurerm" {
+  subscription_id = var.subscription_id # true-development
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+
+  features {}
+}
