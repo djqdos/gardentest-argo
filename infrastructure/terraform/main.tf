f2c6ac8293cfb841b72be98076e5dcf84b1dce34
Richard Bartlett <r.bartlett@openenergymarket.com>
2025-02-07 13:12:59 +0000

    commented out "my-app" from deployment temporarily, so it doesn't get deployed

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -15,6 +15,8 @@ module "kubernetes_cluster" {
   resource_group_name  = module.resource_group.name
   service_principal_id = var.client_id
   password             = var.client_secret
+
+  depends_on=[module.resource_group]
 }
 
 
@@ -22,4 +24,29 @@ module "argocd" {
   source = "./modules/argocd"
   
   depends_on = [module.kubernetes_cluster]
+}
+
+resource "kubernetes_manifest" "application_app_of_apps" {
+  manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind" = "Application"
+    "metadata" = {
+      "name" = "app-of-apps"
+      "namespace": "argocd"
+    }
+    "spec" = {
+      "destination" = {
+        "name" = "in-cluster"
+        "namespace" = "argocd"
+      }
+      "project" = "default"
+      "source" = {
+        "path" = "infrastructure/apps"
+        "repoURL" = "https://github.com/djqdos/gardentest-argo.git"
+        "targetRevision" = "HEAD"
+      }
+    }
+  }
+
+  depends_on = [module.argocd]
 }
